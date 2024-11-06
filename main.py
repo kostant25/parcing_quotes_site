1f76d24d2da4247b8ac6a5a75bf41e4e60f0cf93
Konstantin <goduskospan@gmail.com>
2024-11-06 16:18:12 +0500

    create call parser in main.py

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
+import json
+
+from parser.parser import get_json
+
+# Тип получаемых данных
 ST_ACCEPT = "text/html"
+# Имитируем подключенте через браузер
 ST_USERAGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 YaBrowser/24.10.0.0 Safari/537.36"
 
+# Заголовки для эмоляции реального пользователя
 HEADERS = {"User-Agent": ST_USERAGENT, "Accept": ST_ACCEPT}
 
 if __name__ == "__main__":
-    pass
+    # Запрашиваем все данные с сайта
+    # На выходе получаем массив словарей
+    json_data = get_json("https://quotes.toscrape.com", HEADERS)
+
+    # Вывод общего количества записей
+    print(f"Всего цитат получено: {len(json_data)}")
+
+    # Сохраняем полученные данные в json-файл
+    with open("./output/output.json", "w") as file:
+        json.dump(json_data, file, indent=4)
