b50650c8acc27784779d5dbce2e7617c4ebc455c
Konstantin <goduskospan@gmail.com>
2024-11-06 15:00:44 +0500

    add: main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+ST_ACCEPT = "text/html"
+ST_USERAGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 YaBrowser/24.10.0.0 Safari/537.36"
+
+HEADERS = {"User-Agent": ST_USERAGENT, "Accept": ST_ACCEPT}
+
+if __name__ == "__main__":
+    pass
